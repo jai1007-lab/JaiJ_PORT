3626cc153dbc8998205511c57193bdfb11fcea57
Jai_0710 <74004879+jai1007-lab@users.noreply.github.com>
2023-10-17 16:58:17 -0400

    Updates

---
+++ precache-manifest.d98a6d6c2093b9d86d7a9c63b60366ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aefe31806f25a7f324351b704c7ad4b",
+    "url": "/JaiJ_PORT/index.html"
+  },
+  {
+    "revision": "7a7a05e32a900ab892d6",
+    "url": "/JaiJ_PORT/static/css/main.41111191.chunk.css"
+  },
+  {
+    "revision": "de5766d95991ed20b072",
+    "url": "/JaiJ_PORT/static/js/2.4d50542d.chunk.js"
+  },
+  {
+    "revision": "6f35d6f7b2d27a09d556",
+    "url": "/JaiJ_PORT/static/js/3.bff9dbde.chunk.js"
+  },
+  {
+    "revision": "7a7a05e32a900ab892d6",
+    "url": "/JaiJ_PORT/static/js/main.58b1b1d5.chunk.js"
+  },
+  {
+    "revision": "d01f4822cd919fbc11ec",
+    "url": "/JaiJ_PORT/static/js/runtime~main.d98c15ab.js"
+  }
+]);
