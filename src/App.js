7001060a429ad1ffd51455415a77f89af1332092
Jai_0710 <74004879+jai1007-lab@users.noreply.github.com>
2023-10-17 16:54:10 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Updated import
 import Header from './components/Header/Header';
-import Home from './components/Home/Home';
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact.js';
 
 function App() {
   return (
-    <Router>
+
       <div>
       <Header/>
-        <Routes> {/* Updated to use Routes */}
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+      <About />
+      <Projects />
+      <Contact />
       </div>
-    </Router>
+
   );
 }
 
